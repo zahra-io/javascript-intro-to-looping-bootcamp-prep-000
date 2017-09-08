b47316288df7023c8f674db0f67f96895e8b963d
Zahra Khan <zarssskhan@gmail.com>
2017-09-08 01:30:33 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,8 +24,7 @@ function doWhileLoop(array) {
   }
 
   do {
-    array = array.slice(1);
-  } while (array.length > 0 && maybeTrue());
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
 
-  return array;
-};
+  return array
