6ea68a518595f47c07009c62b9071e2c80cc09da
Zahra Khan <zarssskhan@gmail.com>
2017-09-08 01:10:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,1 @@
 function forLoop(array) {
-  for(var i = 0; i < 25; i++){
-    if(i === 1) {
-      array.push(`I am ${i} strange loop.`)
-    } else {
-      array.push(`I am ${i} strange loops`)
-    }
-  }
-}
